0c6310129ae674cf5e10cacb8d17ba895621718f
Nithin Buggarigani <106420141+bnithin215@users.noreply.github.com>
2025-07-05 00:24:49 +0530

    Add files via upload

---
+++ Employees.sql
@@ -0,0 +1,11 @@
+CREATE TABLE employees (
+    emp_id VARCHAR(10) PRIMARY KEY,
+    name VARCHAR(100),
+    role VARCHAR(50),
+    department VARCHAR(50),
+    joining_date DATE,
+    status VARCHAR(20),
+    leave_days INT,
+    attendance FLOAT
+);
+
